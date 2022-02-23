9ac0327ff8c5d710db58daeeb7a09ead21eae08f
shyamr586 <v.shyamr586@gmail.com>
2022-02-23 13:38:30 +0400

    changed db to postgre

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -129,8 +129,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd7s886195ohaqs',
+        'USER': os.getenv("USER"),
+        'PASSWORD': os.getenv("PASSWORD"),
+        'HOST': 'ec2-3-228-222-169.compute-1.amazonaws.com',
+        'PORT':  '5432',
     }
 }
 

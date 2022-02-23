d0cbbf06a98bd9658d1cdc411cda976d53d824ff
shyamr586 <v.shyamr586@gmail.com>
2022-02-23 11:54:38 +0400

    changed token to env variables

--- portfolio/firstapp/scrape.py
+++ portfolio/firstapp/scrape.py
@@ -1,9 +1,9 @@
 import requests
-from .config import USERNAME,TOKEN
+# from .config import USERNAME,TOKEN
 import os
 
-USERNAME = os.getenv("USERNAME",USERNAME)
-TOKEN = os.getenv("TOKEN",TOKEN)
+USERNAME = os.getenv("USERNAME")
+TOKEN = os.getenv("TOKEN")
 url = f"https://api.github.com/users/{USERNAME}/repos"        
 headers = {"Accept":"application/vnd.github.mercy-preview+json"}        
 REPOS = requests.get(url, headers=headers, auth=(USERNAME,TOKEN)).json()

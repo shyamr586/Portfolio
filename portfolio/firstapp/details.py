b14cc2c1c80d5ebf3e91af266c730d3fd38b6298
shyamr586 <v.shyamr586@gmail.com>
2022-03-06 11:03:44 +0400

    updated details

--- portfolio/firstapp/details.py
+++ portfolio/firstapp/details.py
@@ -9,14 +9,15 @@ short_bio3 = "I'm a passionate and hard working (trust me, I am!) IT grad intere
 
 status = "I am currently looking for a full-time or internship opportunity."
 
-message_bio = "My name is Shyam Ramakrishnan, and I graduated from Middlesex University in April of 2021.\
-        I have an honorary bachelor's degree in Information Technology.\n\nWith the increase in the\
-         development of the IT sector and the many sub-sectors that are branching ever so frequently,\
-        my interest in the field also advanced. After writing my very first script using Python 2.7, I\
-         realized that I had an interest in the field and was very eager to learn more about the attainable\
-         progress. I constantly observe the requirements put forward by the industry to see what skills\
-         are desired and give my best effort to keep up with the standards. \n\nI come across my colleagues as an\
-         honest and hard-working individual who would always be punctual and trustworthy."
+message_bio = "My name is Shyam Ramakrishnan, and I graduated from Middlesex University in June of 2021.\
+        I have a honorary bachelor's degree in B.Sc. Information Technology.\n\nWith the increase in the\
+         progress of the IT sector and the many sub-sectors that are branching ever so frequently,\
+        my interest in the field have also advanced. After writing my very first script using Python 2.7, I\
+         realized that I had an interest in the field and was very eager to learn more ever since. \
+             I constantly observe the requirements put forward by the industry to see what skills\
+         are desired and give my best effort to keep up with the standards.\
+              \n\nI come across my colleagues as a\
+         honest and hard-working individual with solid punctuality and reliability."
     
 skills = [
     "Python 2.7/3+",
